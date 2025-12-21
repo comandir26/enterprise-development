c8f1079994662504c3a2051fd3d81d8968f5cedb
comandir26 <WWolkoW04@yandex.ru>
2025-12-21 18:21:14 +0400

    Add Bikes.Generator project and add kafka in AppHost

--- Bikes/Bikes.AppHost/Program.cs
+++ Bikes/Bikes.AppHost/Program.cs
@@ -1,9 +1,14 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
+var kafka = builder.AddKafka("kafka")
+    .WithKafkaUI() 
+    .WithDataVolume(); 
+
 var mongodb = builder.AddMongoDB("mongodb")
     .WithDataVolume();
 
-_ = builder.AddProject<Projects.Bikes_Api_Host>("bikes-api")
-    .WithReference(mongodb);
+var api = builder.AddProject<Projects.Bikes_Api_Host>("bikes-api")
+    .WithReference(mongodb)
+    .WithReference(kafka); 
 
 builder.Build().Run();
